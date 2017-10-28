cbe972ad344d6f8fd371728b23a9eed6ae9f5bb0
Achref Tlija <achref.tlija@gmail.com>
2017-10-28 23:40:18 +0100

    02/05/2017  V1.08

---
+++ src/AppBundle/Entity/Test.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tlija
+ * Date: 28/10/2017
+ * Time: 23:39
+ */
+
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="test")
+ */
+class Test
+{
+
+}
