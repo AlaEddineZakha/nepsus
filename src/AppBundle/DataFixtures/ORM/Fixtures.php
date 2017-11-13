815b7572f1aded825b0513772a1360f0afcd16e8
Achref Tlija <achref.tlija@gmail.com>
2017-11-13 12:57:18 +0100

    02/05/2017  V1.4 Fixture

--- src/AppBundle/DataFixtures/ORM/Fixtures.php
+++ src/AppBundle/DataFixtures/ORM/Fixtures.php
@@ -1,77 +1,79 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Tlija
+ * Date: 13/11/2017
+ * Time: 12:51
+ */
 
-namespace AppBundle\Controller;
+namespace AppBundle\DataFixtures\ORM;
+
 
 use AppBundle\Entity\Permission;
 use AppBundle\Entity\Role;
 use AppBundle\Entity\RolePermission;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
 
-class InitialisationController extends Controller
+class Fixtures extends Fixture
 {
-
-    /**
-     *@Route("/initialiser", name="initialiserapp")
-     */
-    public function initialiserAction()
+    public function load(ObjectManager $manager)
     {
-        $em = $this->getDoctrine()->getManager();
+
+
         $permission1= new Permission();
         $permission1->setLibele("ajouterclient");
         $permission1->setDescription("Permet d'ajouter un client");
         $permission1->setCreated(new \DateTime());
-        $em->persist($permission1);
+        $manager->persist($permission1);
 
         $permission2= new Permission();
         $permission2->setLibele("modifierclient");
-        $permission2->setDescription("Permet de modifier un client");
+        $permission2->setDescription("Permet d'ajouter un client");
         $permission2->setCreated(new \DateTime());
-        $em->persist($permission2);
+        $manager->persist($permission2);
 
         $permission3= new Permission();
         $permission3->setLibele("supprimerclient");
-        $permission3->setDescription("Permet de supprimer un client");
+        $permission3->setDescription("Permet d'ajouter un client");
         $permission3->setCreated(new \DateTime());
-        $em->persist($permission3);
+        $manager->persist($permission3);
 
         $permission4= new Permission();
         $permission4->setLibele("voirclient");
-        $permission4->setDescription("Permet de voir la liste des clients");
+        $permission4->setDescription("Permet d'ajouter un client");
         $permission4->setCreated(new \DateTime());
-        $em->persist($permission4);
+        $manager->persist($permission4);
+
 
         $role=new Role();
         $role->setNom('Magasinier');
         $role->setDescription('Magasinier');
         $role->setCreated(new \DateTime());
-        $em->persist($role);
-
+        $manager->persist($role);
 
         $rolepermission= new RolePermission();
         $rolepermission->setPermission($permission1);
         $rolepermission->setRole($role);
-        $em->persist($rolepermission);
+        $manager->persist($rolepermission);
 
         $rolepermission= new RolePermission();
         $rolepermission->setPermission($permission2);
         $rolepermission->setRole($role);
-        $em->persist($rolepermission);
+        $manager->persist($rolepermission);
 
         $rolepermission= new RolePermission();
         $rolepermission->setPermission($permission3);
         $rolepermission->setRole($role);
-        $em->persist($rolepermission);
+        $manager->persist($rolepermission);
 
         $rolepermission= new RolePermission();
         $rolepermission->setPermission($permission4);
         $rolepermission->setRole($role);
-        $em->persist($rolepermission);
+        $manager->persist($rolepermission);
 
 
-
-
-        $em->flush();
-        return $this->redirectToRoute('dashboard');
+        $manager->flush();
     }
+
 }
